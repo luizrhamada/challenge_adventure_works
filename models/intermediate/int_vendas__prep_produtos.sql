8542ae33514557672f749dcb033677d0f7cd27f2
Luiz Hamada <luizrhamada@gmail.com>
2025-01-15 22:14:36 +0000

    adicionada configuração para produto e feita a configuracao global

--- models/intermediate/int_vendas__prep_produtos.sql
+++ models/intermediate/int_vendas__prep_produtos.sql
@@ -4,9 +4,9 @@ with
         from {{ ref('stg_erp__product') }}
     )
 
-    , productvendor as (
+    , productsubcategory as (
         select *
-        from {{ ref('stg_erp__productvendor') }}
+        from {{ ref('stg_erp__productsubcategory') }}
     )
 
     , transactionhistory as (
@@ -17,29 +17,14 @@ with
     , increase_product as (
         select
             product.PK_PRODUCT_ID
-            , product.FK_PRODUCT_SUB_CAT_ID
-            , product.FK_PRODUCT_MODEL_ID
             , product.SAFE_STOCK_LEVEL
             , product.REORDER_POINT
-            , product.WEIGHT
             , product.NAME
             , product.PRODUCT_NUMBER
             , product.COLOR
             , product.PRODUCT_LINE
-            , product.SELL_START_DATE
-            , product.SELL_END_DATE
-            , productvendor.FK_BUSINESS_ENTITY_ID
-            , productvendor.STANDARD_PRICE
-            , transactionhistory.PK_TRANSACTION_ID
-            , transactionhistory.FK_REFERENCE_ORDER_LINE_ID
-            , transactionhistory.FK_REFERENCE_ORDER
-            , transactionhistory.TRANSACTION_DATE
-            , transactionhistory.FK_TRANSACTION_TYPE
-            , transactionhistory.FK_QUANTITIY
-            , transactionhistory.FK_ACTUAL_COST
         from product
-        left join transactionhistory on product.pk_product_id = transactionhistory.fk_product_id
-        left join productvendor on product.pk_product_id = productvendor.fk_product_id
+        left join productsubcategory on product.fk_product_sub_cat_id = productsubcategory.pk_product_sub_cat_id
     )
 
 select *
---
+++ models/staging/erp/stg_erp__productsubcategory.sql
@@ -0,0 +1,17 @@
+with
+    fonte_productsubcategory as (
+        select *
+        from {{ source('erp','productsubcategory') }}
+    )
+
+    , renomeado as (
+        select
+            cast(PRODUCTSUBCATEGORYID as int) as pk_product_sub_cat_id
+            , cast(PRODUCTCATEGORYID as int) as fk_product_cat_id
+            , cast(NAME as string) as name
+
+        from fonte_productsubcategory
+    )
+
+select *
+from renomeado
